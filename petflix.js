344cdf2933b95012e83d72675cd9bf78fe4d9a6a
KaiyuanZhu <kzhu4@stanford.edu>
2015-11-29 18:28:56 -0800

    fix the confirmation

--- petflix.js
+++ petflix.js
@@ -23,9 +23,9 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     pet_profile.remove({});
     var bell_bio = "Bella comes from a dog loving family with two young kids. Bella is always excited for a friend to hang out with.";
-    pet_profile.insert({name: "Bella", breed: 'Labrador', rating: 5, age: 4, bio: bell_bio, temperment: 'mild', imgURL : "bella.png"});
+    pet_profile.insert({name: "Bella", breed: 'Labrador', rating: '5star.png', age: 4, bio: bell_bio, temperment: 'mild', imgURL : "bella.png"});
     var max_bio = "Max loves people";
-    pet_profile.insert({name: "Max", breed: 'Golden Retriever', rating: 4, age: 3, bio: max_bio, temperment: 'energetic', imgURL : "max.png"});
+    pet_profile.insert({name: "Max", breed: 'Golden Retriever', rating: '4star.png', age: 3, bio: max_bio, temperment: 'energetic', imgURL : "max.png"});
     Meteor.publish("all pets", function(){
       return pet_profile.find();
     });
@@ -47,6 +47,15 @@ if (Meteor.isClient) {
       return pet_profile.find({});
     }
   });
+
+    Template.confirmation.helpers({
+    pet: function() {
+      var pet =  pet_profile.findOne({name: pet_name});
+      return pet;
+    }
+    
+  });
+
 
 
 }
