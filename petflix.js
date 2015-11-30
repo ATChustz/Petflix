f90cf66174e7a5a99caf10bb3e22dd8da08bf6d8
Austin Chustz <atchustz@stanford.edu>
2015-11-29 17:54:34 -0800

    Routing working. Login and list views linked. Working on detail view.

--- petflix.js
+++ petflix.js
@@ -1,19 +1,29 @@
-    pet_profile = new Mongo.Collection("pet profile");
+Router.route('/', function () {
+  this.render('login');
+});
+
+Router.route('/list', function () {
+  this.render('list');
+});
+
+
+pet_profile = new Mongo.Collection("pet profile");
+var pet_name = "Bella";
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    pet_profile.remove({}); //remove all documents
+    pet_profile.remove({});
     var bell_bio = "Bella comes from a dog loving family with two young kids. Bella is always excited for a friend to hang out with.";
-    pet_profile.insert({name: "Bella", breed: 'Labrador', rating: "5star.png", age: 4, bio: bell_bio, temperment: 'mild', imgURL : "bella.png"});
+    pet_profile.insert({name: "Bella", breed: 'Labrador', rating: 5, age: 4, bio: bell_bio, temperment: 'mild', imgURL : "bella.png"});
+    var max_bio = "Max loves people";
+    pet_profile.insert({name: "Max", breed: 'Golden Retriever', rating: 4, age: 3, bio: max_bio, temperment: 'energetic', imgURL : "max.png"});
     Meteor.publish("all pets", function(){
       return pet_profile.find();
     });
-
-
   });
 }
 if (Meteor.isClient) {
-  var pet_name = "Bella";
   Meteor.subscribe("all pets");
 
 
@@ -24,7 +34,11 @@ if (Meteor.isClient) {
     }
   });
 
-
+  Template.dog.helpers({
+    pets: function () {
+      return pet_profile.find({});
+    }
+  });
 
 
 }
